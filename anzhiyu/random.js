427c41cbed45fe82c5637e67e8eb0b9201fcd867
OuyJay <Oukgds@outlook.com>
2025-05-18 03:41:55 +0800

    dmkl
    dmkl

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/17/hello-world/","2025/05/17/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/05/17/hello-world/","2025/05/18/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
